e432f3c19f83bb0a278d6448c9ef1e2bea5632f7
Nazar Taraniuk <nazartaranyuk@gmail.com>
2025-09-13 20:10:55 +0300

    updated gitignore

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"nazartaraniuk/alertsProject/internal/app/client"
+	// "log"
+	// "nazartaraniuk/alertsProject/internal/app/client"
 	"nazartaraniuk/alertsProject/internal/config"
-	"time"
+	// "time"
 )
 
 const(
@@ -16,14 +16,14 @@ func main() {
 	cfg := config.LoadConfig(CONFIG_PATH)
 	fmt.Println(cfg.Client.APIBaseURL)
 
-	client, err := client.NewClient(
-		cfg.Client.APIBaseURL,
-		time.Second,
-		cfg.Client.APIKey,
-	)
+	// client, err := client.NewClient(
+	// 	cfg.Client.APIBaseURL,
+	// 	time.Second,
+	// 	cfg.Client.APIKey,
+	// )
 
 
-	if err != nil {
-		log.Fatalf("Some error with creating Client, %v", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("Some error with creating Client, %v", err)
+	// }
 }
