153c7acf86c3ce67c73a9bcc7ad132bb8f24802e
Nazar Taraniuk <ntaraniuk@eva.ua>
2025-09-21 19:22:43 +0300

    TASK-3 Fix entities and health handler

--- internal/adapter/handler/health_handler.go
+++ internal/adapter/handler/health_handler.go
@@ -2,18 +2,24 @@ package handler
 
 import (
 	"encoding/json"
+	"nazartaraniuk/alertsProject/internal/domain"
 	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/sirupsen/logrus"
 )
 
+// Health godoc
+// @Summary check server health
+// @Description checks server health and returns result
+// @Tags health
+// Success
 func Health() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		c.Response().Header().Set("Content-Type", "application/json")
-		err := json.NewEncoder(c.Response().Writer).Encode(map[string]string{
-			"status": "ok",
-			"time":   time.Now().Format(time.RFC3339),
+		err := json.NewEncoder(c.Response().Writer).Encode(domain.Health{
+			Status: "ok",
+			Time:   time.Now().Format(time.RFC3339),
 		})
 		if err != nil {
 			logrus.Printf("Cannot encode: %v", err)
--- internal/domain/entity.go
+++ internal/domain/entity.go
@@ -7,6 +7,11 @@ type Error struct {
 	Time    time.Time `json:"time,omitempty"`
 }
 
+type Health struct {
+	Status string `json:"status"`
+	Time   string `json:"time"`
+}
+
 func NewError(message string, time time.Time) Error {
 	return Error{message, time}
 }
